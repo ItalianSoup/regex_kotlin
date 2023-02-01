54c0f4d7a6a78944e21747e8023936bf01c2c847
student-14 <terleckawika123@gmail.com>
2023-02-01 13:25:51 +0100

    porownanie hasel

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -2,12 +2,27 @@ package com.test.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var pass1 = findViewById<EditText>(R.id.passinput).toString()
+    var pass2 = findViewById<EditText>(R.id.passinput2).toString()
+    val check = findViewById<Button>(R.id.button)
+    val text = findViewById<TextView>(R.id.textView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        check.setOnClickListener {
+            if(pass1==pass2){
+                text.setText("match!")
+        }
 
-        
+    }
+
+
     }
 }
+
+
