3c41690aa61bb1f0d979e686329a6a6f346453b1
ItalianSoup <italian.soup.info@gmail.com>
2023-02-05 20:09:59 +0100

    final

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-
+//maciej zdrojewski
 class MainActivity : AppCompatActivity() {
 
     @SuppressLint("ResourceAsColor")
